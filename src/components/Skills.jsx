0f26fd634864fc74de3e5fc46c90bbafee3006f1
David Henry <dah06a@acu.edu>
2021-05-22 18:16:58 -0500

    Working save for carousel images.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, ListGroup, ProgressBar, Carousel } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, ProgressBar, Carousel, Image } from 'react-bootstrap';
+import HtmlBanner from '../images/html-banner.png';
+import CssBanner from '../images/css-banner.png';
+import SassBanner from '../images/sass-banner.png';
+import BootstrapBanner from '../images/bootstrap-banner.png';
+import JavascriptBanner from '../images/javascript-banner.png';
+import JqueryBanner from '../images/jquery-banner.png';
+import ReactBanner from '../images/react-banner.jpeg';
+import ReactNativeBanner from '../images/react-native-banner.png';
+import SqlBanner from '../images/sql-banner.png';
+import NodeBanner from '../images/node-banner.jpg';
+import MongodbBanner from '../images/mongodb-banner.png';
+import MongooseBanner from '../images/mongoose-banner.jpg';
+
 import './Skills.css';
 
 const SkillItem = (props) => {
@@ -79,44 +92,17 @@ class Skills extends Component {
                         </ListGroup>
                     </Col>
                 </Row>
-                <Row className="mt-5">
+                <Row className="my-5">
                     <Col md={8} className="m-auto">
                         <Carousel fade>
                             <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src="holder.js/800x400?text=First slide&bg=373940"
-                                alt="First slide"
+                                <Image
+                                className="w-100"
+                                src={HtmlBanner}
+                                alt="HTML Banner Image"
                                 />
-                                <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </Carousel.Caption>
                             </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src="holder.js/800x400?text=Second slide&bg=282c34"
-                                alt="Second slide"
-                                />
 
-                                <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src="holder.js/800x400?text=Third slide&bg=20232a"
-                                alt="Third slide"
-                                />
-
-                                <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
                         </Carousel>
                     </Col>
                 </Row>
