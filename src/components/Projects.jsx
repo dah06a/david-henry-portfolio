20c122b97aed13afbe52ee5def11a389850e15f8
David Henry <dah06a@acu.edu>
2021-05-25 20:32:38 -0500

    Started projects page with example card.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Container, Row, Col, CardDeck, Card } from 'react-bootstrap';
+import Img from '../images/acu-banner.png';
 import './Projects.css';
 
 function Projects(props) {
     return (
-        <div className="portfolio">
-            Projects
-        </div>
+        <Container fluid className="projects">
+            <Row className="projects-title">
+                <Col className="align-self-center">
+                    <h1>Projects</h1>
+                </Col>
+            </Row>
+            <Row className="m-5">
+                <Col md={4}>
+                    <Card>
+                        <Card.Header>
+                            <Card.Title>Header Example</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Img style={{width: '100%'}} src={Img} />
+                            <Card.Text>Card Text Area</Card.Text>
+                        </Card.Body>
+                        <Card.Footer>Footer Example</Card.Footer>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
