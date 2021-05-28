2d4310d31f3fad37e4c243af8940e675b9606fa2
David Henry <dah06a@acu.edu>
2021-05-27 22:22:04 -0500

    Portrait photos added and home page updated.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import Portrait from '../images/summit-selfie.JPG';
+import { Container, Row, Col } from 'react-bootstrap';
 import './Home.css';
 
 function Home(props) {
     return (
-        <Container fluid className="home">
-            <Row className="h-100 justify-content-center align-content-center">
-                <Col md={6} className="content">
-                    <h1><strong>Hello there, I'm David,</strong></h1>
-                    <Image src={Portrait} roundedCircle className="w-50 m-2" />
-                    <h2>and I'm a <s>math teacher</s> full stack developer.</h2>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            <Container fluid className="home">
+                <Row className="h-100 align-content-center">
+                    <Col style={{color: 'black'}}>
+                        <h1><strong>Hello there, I'm David,</strong></h1>
+                        <h2>and I'm a <s>math teacher</s></h2>
+                        <h2>full stack developer.</h2>
+                    </Col>
+                    <Col className="d-none d-md-block" />
+                </Row>
+            </Container>
+            <Container fluid className="d-md-none mobile-portrait" />
+        </>
     );
 }
 
