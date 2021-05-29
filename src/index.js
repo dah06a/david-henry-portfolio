34dff26176df21d058696b86fb3ace2c77dc3ed3
David Henry <dah06a@acu.edu>
2021-05-29 16:32:04 -0500

    Trying hash routing for github pages deployment routing.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/david-henry-portfolio">
+    <HashRouter basename="/david-henry-portfolio">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
