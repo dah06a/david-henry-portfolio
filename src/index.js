330b842e59e94f599eba778717fe91d4dc1d8759
David Henry <dah06a@acu.edu>
2021-05-30 08:54:53 -0500

    Hash Router basename change.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <App />
     </Router>
   </React.StrictMode>,
