31a5e654f2d1941d6bed58a9731f816757ddf890
David Henry <dah06a@acu.edu>
2021-05-30 09:03:43 -0500

    basename change for routing.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>,
