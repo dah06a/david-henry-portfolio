8c77106a374bd332101f5352945006bd7ed91eb6
David Henry <dah06a@acu.edu>
2021-05-31 10:26:18 -0500

    Updated custom navbar for styling.

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -3,43 +3,51 @@ import { Nav, Navbar } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
 import './CustomNavbar.css';
 
-const baseUrl = 'david-henry-portfolio#/';
-
 function CustomNavbar(props) {
+	const homeRef = React.createRef(null);
+	const aboutRef = React.createRef(null);
+	const skillsRef = React.createRef(null);
+	const projectsRef = React.createRef(null);
+	const resumeRef = React.createRef(null);
+	const contactRef = React.createRef(null);
 
-	console.log(props.location.pathname);
-
-    return (
-        <Navbar collapseOnSelect expand="md" bg="light" sticky="top" className="custom-navbar">
+	return (
+		<Navbar collapseOnSelect expand="md" bg="light" sticky="top">
 
 			<Navbar.Toggle aria-controls="collapse-toggle" />
 
-			<Navbar.Brand><Link to="/">David Henry</Link></Navbar.Brand>
+			<Nav className="m-auto" activeKey={props.location.pathname}>
+				<Nav.Link eventKey="/" className="bootstrap-link" onClick={() => homeRef.current.click()}>
+					<Link replace ref={homeRef} to="/" className="router-link">David Henry</Link>
+				</Nav.Link>
+			</Nav>
 
 			<Navbar.Collapse sticky="top" id="collapse-toggle">
 
 				<Nav className="m-auto" activeKey={props.location.pathname}>
-					<Nav.Link eventKey="/about" className="bootstrap-link">
-						<Link to="/about" className="router-link">About</Link>
+					<Nav.Link eventKey="/about" className="bootstrap-link" onClick={() => aboutRef.current.click()}>
+						<Link replace ref={aboutRef} to="/about" className="router-link">About</Link>
 					</Nav.Link>
-					<Nav.Link eventKey="/skills" className="bootstrap-link">
-						<Link to="/skills" className="router-link">Skills</Link>
+					<Nav.Link eventKey="/skills" className="bootstrap-link" onClick={() => skillsRef.current.click()}>
+						<Link replace ref={skillsRef} to="/skills" className="router-link">Skills</Link>
 					</Nav.Link>
-					<Nav.Link eventKey="/projects" className="bootstrap-link">
-						<Link to="/projects" className="router-link">Projects</Link>
+					<Nav.Link eventKey="/projects" className="bootstrap-link" onClick={() => projectsRef.current.click()}>
+						<Link replace ref={projectsRef} to="/projects" className="router-link">Projects</Link>
 					</Nav.Link>
-					<Nav.Link eventKey="/resume" className="bootstrap-link">
-						<Link to="/resume" className="router-link">Resume</Link>
+					<Nav.Link eventKey="/resume" className="bootstrap-link" onClick={() => resumeRef.current.click()}>
+						<Link replace ref={resumeRef} to="/resume" className="router-link">Resume</Link>
 					</Nav.Link>
 				</Nav>
 
 				<Nav activeKey={props.location.pathname}>
-					<Link to="/contact" eventKey="/contact">Contact</Link>
+					<Nav.Link eventKey="/contact" className="bootstrap-link" onClick={() => contactRef.current.click()}>
+						<Link replace ref={contactRef} to="/contact" className="router-link">Contact</Link>
+					</Nav.Link>
 				</Nav>
 
 			</Navbar.Collapse>
 		</Navbar>
-    );
+	);
 }
 
 export default withRouter(CustomNavbar);
