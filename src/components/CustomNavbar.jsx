00825d7ace90661707ba1e491a736b8d36e4572d
David Henry <dah06a@acu.edu>
2021-06-02 11:14:08 -0500

    Navigation nesting bug fixed.  Links now use NavLink React Router component.

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap';
-import { Link, withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import './CustomNavbar.css';
 
 function CustomNavbar(props) {
-	const homeRef = React.createRef(null);
-	const aboutRef = React.createRef(null);
-	const skillsRef = React.createRef(null);
-	const projectsRef = React.createRef(null);
-	const resumeRef = React.createRef(null);
-	const contactRef = React.createRef(null);
 
 	return (
 		<Navbar collapseOnSelect expand="md" bg="light" sticky="top">
@@ -20,25 +14,13 @@ function CustomNavbar(props) {
 
 			<Navbar.Collapse id="collapse" className="justify-content-end">
 
-				<Nav activeKey={props.location.pathname} onSelect={() => window.scroll(0, 0)}>
-					<Nav.Link eventKey="/" className="bootstrap-link" onClick={() => homeRef.current.click()}>
-						<Link replace ref={homeRef} to="/" className="router-link">Home</Link>
-					</Nav.Link>
-					<Nav.Link eventKey="/about" className="bootstrap-link" onClick={() => aboutRef.current.click()}>
-						<Link replace ref={aboutRef} to="/about" className="router-link">About</Link>
-					</Nav.Link>
-					<Nav.Link eventKey="/skills" className="bootstrap-link" onClick={() => skillsRef.current.click()}>
-						<Link replace ref={skillsRef} to="/skills" className="router-link">Skills</Link>
-					</Nav.Link>
-					<Nav.Link eventKey="/projects" className="bootstrap-link" onClick={() => projectsRef.current.click()}>
-						<Link replace ref={projectsRef} to="/projects" className="router-link">Projects</Link>
-					</Nav.Link>
-					<Nav.Link eventKey="/resume" className="bootstrap-link" onClick={() => resumeRef.current.click()}>
-						<Link replace ref={resumeRef} to="/resume" className="router-link">Resume</Link>
-					</Nav.Link>
-					<Nav.Link eventKey="/contact" className="bootstrap-link" onClick={() => contactRef.current.click()}>
-						<Link replace ref={contactRef} to="/contact" className="router-link">Contact</Link>
-					</Nav.Link>
+				<Nav>
+					<NavLink to="/" className="link" activeClassName="link-active" exact onClick={() => window.scroll(0, 0)}>Home</NavLink>
+					<NavLink to="/about" className="link" activeClassName="link-active" onClick={() => window.scroll(0, 0)}>About</NavLink>
+					<NavLink to="/skills" className="link" activeClassName="link-active" onClick={() => window.scroll(0, 0)}>Skills</NavLink>
+					<NavLink to="/projects" className="link" activeClassName="link-active" onClick={() => window.scroll(0, 0)}>Projects</NavLink>
+					<NavLink to="/resume" className="link" activeClassName="link-active" onClick={() => window.scroll(0, 0)}>Resume</NavLink>
+					<NavLink to="/contact" className="link" activeClassName="link-active" onClick={() => window.scroll(0, 0)}>Contact</NavLink>
 				</Nav>
 
 			</Navbar.Collapse>
