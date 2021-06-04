bc02b0fc1d9586bca2f32bafef942e39c0bea9bf
David Henry <dah06a@acu.edu>
2021-06-04 08:41:25 -0500

    Skills page updated with progress animation and css changes.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, ListGroup, ProgressBar, Carousel, Image } from 'react-bootstrap';
+import { Container, Row, Col, Carousel, Image, ProgressBar } from 'react-bootstrap';
 
 import BootstrapBadge from '../images/bootstrap-badge.png';
 import ReactBadge from '../images/react-badge.png';
@@ -45,28 +45,38 @@ import './Skills.css';
 class Skills extends Component {
     state = {
         progress: 0,
+        skillItems: ['Html', 'CSS', 'JavaScript', 'JQuery', 'React', 'JSX', 'Redux', 'Bootstrap', 'Sass', 'React-Native', 'Node.JS Express', 'MongoDB', 'Mongoose', 'SQL', 'Nucamp Bootcamp']
     }
 
     componentDidMount() {
 
         const progressInterval = setInterval(() => {
-            if (this.state.progress < 10) this.setState({progress: this.state.progress+1});
+            if (this.state.progress < 15) this.setState({progress: this.state.progress+1});
             else clearInterval(progressInterval);
-        }, 200);
+        }, 300);
 
     }
 
     render(props) {
-        return (
-            <>
-                <Container fluid>
-                    <Row className="skills-title">
-                        <Col className="align-self-center">
-                            <h1>Skills and Technologies</h1>
-                        </Col>
-                    </Row>
-                </Container>
+        let skillsContent = (
+            <Container style={{height: '50vh'}}>
+                <Row style={{height: '100%'}}>
+                    <Col style={{alignSelf: 'center'}}>
+                        <ProgressBar
+                            animated
+                            variant='primary'
+                            style={{height: '50px', fontSize: '24px'}}
+                            label={this.state.skillItems[this.state.progress]}
+                            now={this.state.progress + 1}
+                            max={15}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        );
 
+        if (this.state.progress >= 15) {
+            skillsContent = (
                 <Container>
                     <Row className="skill-area">
                         <Col xs={12} className="mt-5 align-self-center">
@@ -76,26 +86,26 @@ class Skills extends Component {
                         <Col sm={12} md={6} lg={4}>
                             <Row className="my-5">
                                 <Col xs={6}>
-                                    <Image className="nucamp-badge" src={BootstrapBadge} />
+                                    <Image roundedCircle className="nucamp-badge" src={BootstrapBadge} />
                                 </Col>
                                 <Col xs={6}>
-                                    <Image className="nucamp-badge" src={ReactBadge} />
+                                    <Image roundedCircle className="nucamp-badge" src={ReactBadge} />
                                 </Col>
                             </Row>
                             <Row className="my-5">
                                 <Col xs={6}>
-                                    <Image className="nucamp-badge" src={ReactNativeBadge} />
+                                    <Image roundedCircle className="nucamp-badge" src={ReactNativeBadge} />
                                 </Col>
                                 <Col xs={6}>
-                                    <Image className="nucamp-badge" src={BackendBadge} />
+                                    <Image roundedCircle className="nucamp-badge" src={BackendBadge} />
                                 </Col>
                             </Row>
                         </Col>
-                        <Col sm={12} md={4}>
+                        <Col sm={12} md={6} lg={4}>
                             <Image rounded className="certificate" src={FrontendCertificate} />
                         </Col>
 
-                        <Col sm={12} md={4}>
+                        <Col sm={12} md={6} lg={4}>
                             <Image rounded className="certificate" src={BackendCertificate} />
                         </Col>
                     </Row>
@@ -166,10 +176,6 @@ class Skills extends Component {
                             <hr style={{borderColor: "grey"}} />
                         </Col>
                         <Col md={2}>
-                            <Image roundedCircle src={SqlIcon} className="icons" style={{border: "1px solid lightgrey"}} />
-                            <h5>SQL</h5>
-                        </Col>
-                        <Col md={2}>
                             <Image src={NodeIcon} className="icons" />
                             <h5>Node Express</h5>
                         </Col>
@@ -180,6 +186,10 @@ class Skills extends Component {
                         <Col md={2}>
                             <Image roundedCircle src={MongooseIcon} className="icons" style={{border: "1px solid lightgrey"}} />
                             <h5>Mongoose</h5>
+                        </Col>
+                        <Col md={2}>
+                            <Image roundedCircle src={SqlIcon} className="icons" style={{border: "1px solid lightgrey"}} />
+                            <h5>SQL</h5>
                         </Col>
                     </Row>
                     <Row className="skill-area">
@@ -235,6 +245,18 @@ class Skills extends Component {
                         </Col>
                     </Row>
                 </Container>
+            );
+        }
+        return (
+            <>
+                <Container fluid>
+                    <Row className="skills-title">
+                        <Col className="align-self-center">
+                            <h1>Skills and Technologies</h1>
+                        </Col>
+                    </Row>
+                </Container>
+                {skillsContent}
             </>
         );
     }
