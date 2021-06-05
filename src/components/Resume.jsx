b1a3acd78b89c9cd6b5fd4dbf30248a9e4d0e44d
David Henry <dah06a@acu.edu>
2021-06-05 15:45:31 -0500

    Resume added in pieces using animations.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
-import ResumeImg from '../images/resume.png';
+import { FadeTransform, Stagger } from 'react-animation-components'
+
+import ResumeTitle from '../images/resume-title.png';
+import ResumeExperience from '../images/resume-experience.png';
+import ResumeTools from '../images/resume-tools.png';
+import ResumeWork from '../images/resume-work.png';
+import ResumeEducation from '../images/resume-education.png';
 
 import './Resume.css';
 
 function Resume(props) {
+
+    const resumeItems = [ResumeTitle, ResumeExperience, ResumeTools, ResumeWork, ResumeEducation];
+    const resume = (
+        <Stagger in delay={300} duration={1000}>
+            {resumeItems.map(item => (
+                <FadeTransform
+                    in
+                    transformProps={{
+                        enterTransform: 'translateX(100px)',
+                        exitTransform: 'translateX(-100px)'
+                    }}
+                >
+                    <Col>
+                        <Image src={item} className="resume-item" />
+                    </Col>
+                </FadeTransform>
+            ))}
+        </Stagger>
+    );
 
     return (
         <Container fluid>
@@ -14,9 +39,7 @@ function Resume(props) {
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    <Image src={ResumeImg} />
-                </Col>
+                {resume}
             </Row>
         </Container>
     );
