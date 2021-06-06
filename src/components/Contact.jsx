7fe70fd31138405bba5e5a3c59e0f9b03e1697a2
David Henry <dah06a@acu.edu>
2021-06-05 20:04:17 -0500

    Contact information added and footer contact connections fixed.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 import './Contact.css';
 
 function Contact(props) {
     return (
-        <div className="contact">
-            <h1>Contact Me</h1>
-            Work in progress...
-        </div>
+        <Container fluid className="contact">
+            <Row className="contact-title">
+                <Col className="align-self-center">
+                    <h1>Contact</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} md={8} lg={6} className="m-auto">
+                    <ListGroup className="text-left">
+                        <ListGroup.Item className="contact-item">
+                            <h5><FontAwesomeIcon icon={faEnvelope} /><a href="mailto: dah06a@acu.edu" > dah06a@acu.edu</a></h5>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="contact-item">
+                            <h5><FontAwesomeIcon icon={faPhone} /><a href="tel:+1-325-260-5182"> (325) 260 - 5182</a></h5>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="contact-item">
+                            <h5><FontAwesomeIcon icon={faLinkedin} /><a href="https://www.linkedin.com/in/david-henry-653454203" target="_blank" rel="noreferrer"> LinkedIn.com</a></h5>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="contact-item">
+                            <h5><FontAwesomeIcon icon={faGithub} /><a href="https://github.com/dah06a" target="_blank" rel="noreferrer"> Github.com</a></h5>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="contact-item">
+                            <h5><FontAwesomeIcon icon={faFacebook} /><a href="https://www.facebook.com/david.henry.7399786" target="_blank" rel="noreferrer"> Facebook.com</a></h5>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithubSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
 
 import './Footer.css';
 
@@ -10,16 +10,16 @@ function Footer(props) {
         <Container fluid >
             <Row className="footer align-items-center">
                 <Col md={6}>
-                    <h5><a href="tel:+1-325-260-5182" className="m-auto">+1-325-260-5182</a></h5>
+                    <h5><a href="tel:+1-325-260-5182" className="m-auto">(325) 260 - 5182</a></h5>
                 </Col>
                 <Col md={6} className="social-icons">
-                    <a href="https://linkedin.com" className="m-2">
+                    <a href="https://www.linkedin.com/in/david-henry-653454203/" target="_blank" rel="noreferrer" className="m-2">
                         <FontAwesomeIcon icon={faLinkedin} size="2x" />
                     </a>
-                    <a href="https://github.com/dah06a" className="m-2">
+                    <a href="https://github.com/dah06a" target="_blank" rel="noreferrer" className="m-2">
                         <FontAwesomeIcon icon={faGithubSquare} size="2x" />
                     </a>
-                    <a href="https://facebook.com" className="m-2">
+                    <a href="https://www.facebook.com/david.henry.7399786" target="_blank" rel="noreferrer" className="m-2">
                         <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
                     </a>
                 </Col>
